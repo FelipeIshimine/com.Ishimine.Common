924ee858f1b2fb0552dceee924cc3a263ed3fc23
Ishimine <felipecalafate@hotmail.com>
2021-09-16 17:29:52 -0300

    Fix

--- Runtime/UI/AnimatedContainer.cs
+++ Runtime/UI/AnimatedContainer.cs
@@ -213,6 +213,8 @@ public class AnimatedContainer : MonoBehaviour
         }
         else
         {
+            canvasGroup.blocksRaycasts = true;
+            canvasGroup.interactable = true;
             postAction?.Invoke();
         }
     }
