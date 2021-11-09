75980a1a094a96aee21e6d92bcd5b5acf3283f46
Ishimine <felipecalafate@hotmail.com>
2021-11-09 13:04:46 -0300

    Async Utilities

---
+++ Runtime/Async/AsyncUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class AsyncUtils
+{
+	public static Task WaitAsync(float waitTime) => WaitAsync(waitTime, CancellationToken.None);
+	
+	public static async Task WaitAsync(float waitTime, CancellationToken cancellationToken)
+	{
+		float endTime = Time.time + waitTime;
+		while (Time.time < endTime)
+		{
+			if(cancellationToken.IsCancellationRequested)
+				cancellationToken.ThrowIfCancellationRequested();
+			else
+				await Task.Yield();
+		}
+	}
+	
+	public static async Task WaitRealtimeAsync(float duration) => await Task.Delay((int)(duration * 1000));
+}
