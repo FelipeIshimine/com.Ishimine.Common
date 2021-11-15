897f28d0d3d26bf1b010deebc08e9603613cb38b
Ishimine <felipecalafate@hotmail.com>
2021-11-15 17:41:43 -0300

    GlobalUpdate Refact

--- Runtime/Mics/GlobalUpdate.cs
+++ Runtime/Mics/GlobalUpdate.cs
@@ -3,78 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class GlobalUpdate : MonoBehaviour
+public class GlobalUpdate : BaseMonoSingleton<GlobalUpdate>
 {
-    private static bool initialized = false;
-
-    private static Action globalUpdateEvent;
-    public static Action OnUpdateEvent
-    {
-        get 
-        {
-            Initialize();
-            return globalUpdateEvent; 
-        }
-        set { globalUpdateEvent = value; }
-    }
-
-    private static Action globalFixedUpdateEvent;
-    public static Action OnFixedUpdateEvent
-    {
-        get { 
-            Initialize();
-            return globalFixedUpdateEvent; }
-        set { globalFixedUpdateEvent = value; }
-    }
-
-    private static Action globalLateUpdateEvent;
-    public static Action OnLateUpdateEvent
-    {
-        get { 
-            Initialize();
-            return globalLateUpdateEvent; }
-        set { globalLateUpdateEvent = value; }
-    }
-
-    private static GlobalUpdate instance = null;
+    public static event Action OnUpdateEvent;
+    public static event  Action OnFixedUpdateEvent;
+    public static event  Action OnLateUpdateEvent;
 
     [RuntimeInitializeOnLoadMethod]
     private static void Initialize()
     {
-        if (initialized) return;
-        initialized = true;
-        instance = new GameObject().AddComponent<GlobalUpdate>();
-        DontDestroyOnLoad(instance);
-        instance.gameObject.name = "Global Update";
-
-        globalUpdateEvent = null;
-        globalFixedUpdateEvent = null;
-        globalLateUpdateEvent = null;
+        var go = new GameObject().AddComponent<GlobalUpdate>();
+        go.name = "Global Update";
     }
 
-    public static void _StartCoroutine(IEnumerator rutine)
-    {
-        instance.StartCoroutine(rutine);
-    }
+    private void Update() =>  OnUpdateEvent?.Invoke();
+    private void LateUpdate() =>OnLateUpdateEvent?.Invoke();
+    private void FixedUpdate() => OnFixedUpdateEvent?.Invoke();
 
-
-    private void Update()
-    {
-        OnUpdateEvent?.Invoke();
-    }
-
-    private void LateUpdate()
-    {
-        OnLateUpdateEvent?.Invoke();
-    }
-
-    private void FixedUpdate()
-    {
-        OnFixedUpdateEvent?.Invoke();
-    }
-
-    internal static void _StartCoroutine(object transitionOut)
-    {
-        throw new NotImplementedException();
-    }
 }
