f2a83b4c84a7729ad0bd05c8b80167e87ed71e63
Ishimine <felipecalafate@hotmail.com>
2021-12-07 13:03:33 -0300

    Agregadas Colas de eventos en Global Update

--- Runtime/Mics/GlobalUpdate.cs
+++ Runtime/Mics/GlobalUpdate.cs
@@ -11,6 +11,10 @@ public class GlobalUpdate : BaseMonoSingleton<GlobalUpdate>
     public static event  Action OnFixedUpdateEvent;
     public static event  Action OnLateUpdateEvent;
 
+    public static readonly Queue<Action> UpdateEventQueue = new Queue<Action>();
+    public static readonly Queue<Action> LateUpdateEventQueue = new Queue<Action>();
+    public static readonly Queue<Action> FixedUpdateEventQueue = new Queue<Action>();
+
     [RuntimeInitializeOnLoadMethod]
     private static void Initialize()
     {
@@ -18,9 +22,27 @@ public class GlobalUpdate : BaseMonoSingleton<GlobalUpdate>
         go.name = "Global Update";
     }
 
-    private void Update() =>  OnUpdateEvent?.Invoke();
-    private void LateUpdate() =>OnLateUpdateEvent?.Invoke();
-    private void FixedUpdate() => OnFixedUpdateEvent?.Invoke();
+    private void Update()
+    {
+        while (UpdateEventQueue.Count > 0)
+            UpdateEventQueue.Dequeue().Invoke();
+        OnUpdateEvent?.Invoke();
+    }
+
+    private void LateUpdate()
+    {
+        while (LateUpdateEventQueue.Count > 0)
+            LateUpdateEventQueue.Dequeue().Invoke();
+        OnLateUpdateEvent?.Invoke();
+    }
+    private void FixedUpdate()
+    {
+        while (FixedUpdateEventQueue.Count > 0)
+            FixedUpdateEventQueue.Dequeue().Invoke();
+        OnFixedUpdateEvent?.Invoke();
+    }
     private void OnApplicationPause(bool value) => OnApplicationPauseEvent?.Invoke(value);
     private void OnApplicationFocus(bool value) => OnApplicationFocusEvent?.Invoke(value);
+    
+    
 }
