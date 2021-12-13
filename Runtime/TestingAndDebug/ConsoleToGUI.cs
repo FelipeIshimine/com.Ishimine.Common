45eb2712159ad70dd3c3bc27d8617125256ade69
Nicolás Alejandro Feroglio Berman <nicoferoglio@gmail.com>
2021-12-13 11:25:33 -0300

    Runtime console in build.

---
+++ Runtime/TestingAndDebug/ConsoleToGUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ConsoleToGUI : MonoBehaviour
+{
+	#if !UNITY_EDITOR
+	static string myLog = "";
+	private string output;
+	private string stack;
+	private bool showConsole;
+
+	void OnEnable()
+	{
+		Application.logMessageReceived += Log;
+	}
+
+	void OnDisable()
+	{
+		Application.logMessageReceived -= Log;
+	}
+
+	public void Log(string logString, string stackTrace, LogType type)
+	{
+		output = logString;
+		stack = stackTrace;
+		myLog = output + "\n\n" + myLog;
+		if (myLog.Length > 5000)
+		{
+			myLog = myLog.Substring(0, 4000);
+		}
+	}
+
+	void OnGUI()
+	{
+		showConsole = GUI.Toggle(new Rect(Screen.width - 30, Screen.height - 30, 30, 30), showConsole, "");
+
+		if (showConsole)
+		{
+			myLog = GUI.TextArea(new Rect(10, Screen.height / 2, Screen.width - 20, Screen.height), myLog);
+		}
+	}
+	#endif
+}
