2fafab4bc67a873cfe338c7a3fe522fbfbcfd9b7
Ishimine <felipecalafate@hotmail.com>
2021-12-20 11:08:01 -0300

    SceneUtils

--- Runtime/UnityExtensions/UnityExtensions.cs
+++ Runtime/UnityExtensions/UnityExtensions.cs
@@ -730,9 +730,19 @@ public static class MaterialExtensions
         material.EnableKeyword("_ALPHABLEND_ON");
         material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
     }
-
 }
 
+public static class SceneUtils
+{
+    public static UnityEngine.SceneManagement.Scene[] GetAllOpenedScenes()
+    {
+        int countLoaded = UnityEngine.SceneManagement.SceneManager.sceneCount;
+        UnityEngine.SceneManagement.Scene[] loadedScenes = new UnityEngine.SceneManagement.Scene[countLoaded];
+        for (int i = 0; i < countLoaded; i++)
+            loadedScenes[i] = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i);
+        return loadedScenes;
+    }
+}
 
 public static class UnityStringExtensions
 {
