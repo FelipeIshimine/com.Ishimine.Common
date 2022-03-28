4278abce4538fb892509bc67665c62a596f6a4c8
Ishimine <felipecalafate@hotmail.com>
2022-03-28 16:28:51 -0300

    Debug Activate Change

--- Runtime/TestingAndDebug/DebugActivate.cs
+++ Runtime/TestingAndDebug/DebugActivate.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 
 public class DebugActivate : MonoBehaviour
 {
-    public void Awake()=> Debug.Log($"||| DebugActivate: AWAKE {this}");
-    public void Start()=> Debug.Log($"||| DebugActivate: START {this}");
-    public void OnEnable()=> Debug.Log($"||| DebugActivate: ON ENABLE {this}");
-    public void OnDisable() => Debug.Log($"||| DebugActivate: ON DISABLE {this}");
-
-    public void OnDestroy() => Debug.Log($"||| DebugActivate: OnDestroy {this}");
+    public void Awake()=> Debug.Log($"||| DebugActivate: AWAKE {transform.GetHierarchyAsString()}");
+    public void Start()=> Debug.Log($"||| DebugActivate: START {transform.GetHierarchyAsString()}");
+    public void OnEnable()=> Debug.Log($"||| DebugActivate: ON ENABLE {transform.GetHierarchyAsString()}");
+    public void OnDisable() => Debug.Log($"||| DebugActivate: ON DISABLE {transform.GetHierarchyAsString()}");
+    public void OnDestroy() => Debug.Log($"||| DebugActivate: OnDestroy {transform.GetHierarchyAsString()}");
 }
