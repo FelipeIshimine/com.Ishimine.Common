55b98244185d5f3c5b631a43fd271ce2f01c6b5b
Ishimine <felipecalafate@hotmail.com>
2022-03-31 15:40:02 -0300

    ObjectExtensions

---
+++ Runtime/UnityExtensions/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class ObjectExtensions
+{
+    public static bool IsNameUsed<T>(this T source) where T : Object
+    {
+        var list = new List<T>(Object.FindObjectsOfType<T>());
+        list.Remove(source);
+        return list.Find(x => x.name == source.name);
+    }
+}
