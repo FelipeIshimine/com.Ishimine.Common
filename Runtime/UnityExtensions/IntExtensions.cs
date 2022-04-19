038b6fb334066353c71452895a125fe0d12094e4
Ishimine <felipecalafate@hotmail.com>
2022-04-19 17:55:16 -0300

    IntExtensions

---
+++ Runtime/UnityExtensions/IntExtensions.cs
@@ -0,0 +1,13 @@
+﻿public static class IntExtensions
+{
+    public static string ToStringWithUnitPrefixes(this int value)
+    {
+        if (value >= 1000000000)
+            return $"{(float)value / 10000000000:0.0}G";
+        if (value >= 1000000)
+            return $"{(float)value / 10000000:0.0}M";
+        if (value >= 1000)
+            return $"{(float)value / 1000:0.0}K";
+        return value.ToString();
+    }
+}
