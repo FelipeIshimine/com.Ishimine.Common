582feda5255f5487542feaa95fc02a540c438568
Ishimine <felipecalafate@hotmail.com>
2022-04-22 17:45:06 -0300

    Agregado parametro de formato para el IntExtensions.ToStringWithUnitPrefixes

--- Runtime/UnityExtensions/IntExtensions.cs
+++ Runtime/UnityExtensions/IntExtensions.cs
@@ -1,13 +1,13 @@
 ﻿public static class IntExtensions
 {
-    public static string ToStringWithUnitPrefixes(this int value)
+    public static string ToStringWithUnitPrefixes(this int value, string format = "G")
     {
         if (value >= 1000000000)
-            return $"{(float)value / 10000000000:0.0}G";
+            return $"{((float)value / 10000000000).ToString(format)}G";
         if (value >= 1000000)
-            return $"{(float)value / 10000000:0.0}M";
+            return $"{((float)value / 10000000).ToString(format)}M";
         if (value >= 1000)
-            return $"{(float)value / 1000:0.0}K";
+            return $"{((float)value / 1000).ToString(format)}K";
         return value.ToString();
     }
 }
