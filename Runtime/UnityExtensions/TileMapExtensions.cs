10abb49176b48a74814d68c9a2721a166ecb10db
Ishimine <felipecalafate@hotmail.com>
2022-04-24 12:32:09 -0300

    New Extensions

---
+++ Runtime/UnityExtensions/TileMapExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class TileMapExtensions
+{
+    public static Vector3Int[] FindAllCoordinates(this Tilemap tilemap, TileBase baseTile)
+    {
+        BoundsInt boundsInt = tilemap.cellBounds;
+        List<Vector3Int> coordinates = new List<Vector3Int>();
+
+        for (int z = boundsInt.zMin; z < boundsInt.zMax; z++)
+        for (int y = boundsInt.yMin; y < boundsInt.yMax; y++)
+        for (int x = boundsInt.xMin; x < boundsInt.xMax; x++)
+            coordinates.Add(new Vector3Int(x,y,z));
+
+        return coordinates.ToArray();
+    }
+}
