685b38374f889eb573a7cffc4a00c75d0a68d01a
Ishimine <felipecalafate@hotmail.com>
2022-05-30 14:59:42 -0300

    Screenshot

---
+++ Editor/ScreenShot.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class ScreenShot
+{
+    private static string DirectoryName = "Screenshots";
+    private static string FileName => $"Screenshots_{DateTime.Now.ToString(CultureInfo.InvariantCulture).Replace("/","_").Replace(":","-")}.png";
+    
+    [MenuItem("Tools/Take Screenshot")]
+    public static void TakeScreenshot()
+    {
+        //DirectoryInfo screenshotDirectory = Directory.CreateDirectory(DirectoryName);
+        string fullPath = Path.Combine( Application.dataPath, FileName);
+        ScreenCapture.CaptureScreenshot(fullPath);
+        Debug.Log(fullPath);
+        AssetDatabase.SaveAssets();
+    }
+}
