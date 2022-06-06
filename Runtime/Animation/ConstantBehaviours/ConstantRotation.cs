873eb05f97d6f7a42f86496726d9d0a0dc785f0d
Ishimine <felipecalafate@hotmail.com>
2022-06-06 15:54:06 -0300

    AddedLocal Rotation Option

--- Runtime/Animation/ConstantBehaviours/ConstantRotation.cs
+++ Runtime/Animation/ConstantBehaviours/ConstantRotation.cs
@@ -8,9 +8,14 @@ public class ConstantRotation : ConstantBehaviour
     public Vector3 offset = new Vector3();
     public Vector3 axisSpeed = new Vector3(0,0,1);
 
+    public bool useLocalRotation;
 
     protected override void Process(float nTime)
     {
-        transform.rotation = Quaternion.Euler(offset.x + nTime * axisSpeed.x, offset.y + nTime * axisSpeed.y, offset.z + nTime * axisSpeed.z);
+        if(useLocalRotation)
+            transform.localRotation = Quaternion.Euler(offset.x + nTime * axisSpeed.x, offset.y + nTime * axisSpeed.y, offset.z + nTime * axisSpeed.z);
+        else
+            transform.rotation = Quaternion.Euler(offset.x + nTime * axisSpeed.x, offset.y + nTime * axisSpeed.y, offset.z + nTime * axisSpeed.z);
+        
     }
 }
