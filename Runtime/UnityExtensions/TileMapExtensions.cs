41e0f2335a73ca6f9de7a31aa2ab9424e7ebc82b
Felipe Ishimine <felipeishimine@gmail.com>
2022-06-17 18:18:50 -0300

    Tilemap Extensions

--- Runtime/UnityExtensions/TileMapExtensions.cs
+++ Runtime/UnityExtensions/TileMapExtensions.cs
@@ -19,4 +19,52 @@ public static class TileMapExtensions
         }
         return coordinates.ToArray();
     }
+
+
+    public static Vector3Int[] GetAllTileCoordinates(this Tilemap tilemap)
+    {
+        List<Vector3Int> coordinates = new List<Vector3Int>();
+        BoundsInt bound = tilemap.cellBounds;
+        for (int z = bound.zMin; z < bound.zMax; z++)
+        {
+            for (int y = bound.yMin; y < bound.yMax; y++)
+            {
+                for (int x = bound.xMin; x < bound.xMax; x++)
+                {
+                    var coordinate = new Vector3Int(x, y, z);
+                    if (tilemap.HasTile(coordinate))
+                        coordinates.Add(coordinate);
+                }
+            }
+        }
+
+        return coordinates.ToArray();
+    }
+
+    public static void ApplyOffset(this Tilemap tilemap, Vector2Int offset) =>
+        ApplyOffset(tilemap, new Vector3Int(offset.x, offset.y));
+
+    public static void ApplyOffset(this Tilemap tilemap, Vector3Int offset)
+    {
+        Dictionary<Vector3Int, TileBase> _allTiles = new Dictionary<Vector3Int, TileBase>();
+
+        BoundsInt bound = tilemap.cellBounds;
+        for (int z = bound.zMin; z < bound.zMax; z++)
+        {
+            for (int y = bound.yMin; y < bound.yMax; y++)
+            {
+                for (int x = bound.xMin; x < bound.xMax; x++)
+                {
+                    var coordinate = new Vector3Int(x, y, z);
+                    var tile = tilemap.GetTile(coordinate);
+                    if (tile) _allTiles.Add(coordinate, tile);
+                }
+            }
+        }
+        
+        
+        tilemap.ClearAllTiles();
+        foreach (var pair in _allTiles)
+            tilemap.SetTile(pair.Key + offset,pair.Value );
+    }
 }
