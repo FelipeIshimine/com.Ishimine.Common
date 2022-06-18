1f93b9477dc25a6f8c2e2258c3817362cd89ca13
Felipe Ishimine <felipeishimine@gmail.com>
2022-06-17 23:46:45 -0300

    Tilemap Extensions

--- Runtime/UnityExtensions/TileMapExtensions.cs
+++ Runtime/UnityExtensions/TileMapExtensions.cs
@@ -1,9 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public static class TileMapExtensions
 {
+    public static Vector3Int[] FindAllCoordinates(this Tilemap tilemap, Func<TileBase,bool> validator)
+    {
+        BoundsInt boundsInt = tilemap.cellBounds;
+        List<Vector3Int> coordinates = new List<Vector3Int>();
+
+        for (int z = boundsInt.zMin; z < boundsInt.zMax; z++)
+        for (int y = boundsInt.yMin; y < boundsInt.yMax; y++)
+        for (int x = boundsInt.xMin; x < boundsInt.xMax; x++)
+        {
+            var coordinate = new Vector3Int(x, y, z);
+            if(validator.Invoke(tilemap.GetTile(coordinate)))
+                coordinates.Add(coordinate);
+        }
+        return coordinates.ToArray();
+    }
+    
     public static Vector3Int[] FindAllCoordinates(this Tilemap tilemap, TileBase baseTile)
     {
         BoundsInt boundsInt = tilemap.cellBounds;
