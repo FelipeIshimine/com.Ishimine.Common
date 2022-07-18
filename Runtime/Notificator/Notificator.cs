4a4ec96d1a2893a3bb771e0d47148147132d3e37
FelipeIshimine <felipeishimine@gmail.com>
2022-07-18 13:40:38 -0300

    Notificator

---
+++ Runtime/Notificator/Notificator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Notificator<T, TB>
+{
+    private readonly Dictionary<T, Action<TB>> _events = new Dictionary<T, Action<TB>>();
+
+    public void Register(T key, Action<TB> nCallback)
+    {
+        _events.TryGetValue(key, out var callback);
+        callback -= nCallback;
+        callback += nCallback;
+        _events[key] = callback;
+    }
+
+    public void Unregister(T key, Action<TB> nCallback)
+    {
+        _events.TryGetValue(key, out var callback);
+        callback -= nCallback;
+        _events[key] = callback;
+    }
+
+    public void Send(T id, TB value)
+    {
+        if(_events.TryGetValue(id, out Action<TB> eventValue))
+            eventValue?.Invoke(value);
+    }
+
+    public void SendAll(TB value)
+    {
+        foreach (Action<TB> eventsValue in _events.Values)
+            eventsValue?.Invoke(value);
+    }
+}
