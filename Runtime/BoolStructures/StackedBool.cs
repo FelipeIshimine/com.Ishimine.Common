3f07c4ea1371584d09121e22082daa7fef7542fe
Felipe Ishimine <Felipeishimine@gmail.com>
2022-08-16 12:02:18 -0300

    Stacked Bool Changes

--- Runtime/BoolStructures/StackedBool.cs
+++ Runtime/BoolStructures/StackedBool.cs
@@ -1,13 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Unity.Plastic.Newtonsoft.Json.Serialization;
+using Action = Unity.Plastic.Newtonsoft.Json.Serialization.Action;
 
 public class StackedBool
 {
+    public event Action<bool> OnChange;
+    
     private readonly Dictionary<object, bool> _values = new Dictionary<object, bool>();
     private readonly List<object> _stack = new List<object>();
+
+    public StackedBool(bool defaultValue)
+    {
+        Set(defaultValue,this);
+    }
+    
+    public StackedBool(bool defaultValue, Action<bool> onChangeCallback)
+    {
+        Set(defaultValue,this);
+        OnChange = onChangeCallback;
+    }
+
+    public bool Contains(object value) => _values.ContainsKey(value);
+    
     public void Set(bool value, object source)
     {
+        if (Contains(source))
+            throw new Exception($"The object {source} already has a value in the stack");
+
+        var old = Get(); 
         _values[source] = value;
         _stack.Insert(0, source);
+        if(old != value) OnChange?.Invoke(value);
     }
 
     public bool Get() => _values[_stack[0]];
@@ -17,5 +41,7 @@ public class StackedBool
         _values.Remove(source);
         _stack.RemoveAt(_stack.IndexOf(source));
     }
+    
+    public static implicit operator bool(StackedBool source) => source.Get();
 
 }
