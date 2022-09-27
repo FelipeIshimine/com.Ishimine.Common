2a2491a405f7a7b0c664d77c95f10d91428c88f4
Felipe Ishimine <felipeishimine@gmail.com>
2022-09-26 22:47:20 -0300

    Extednsions

--- Runtime/UnityExtensions/Vector2IntExtensions.cs
+++ Runtime/UnityExtensions/Vector2IntExtensions.cs
@@ -13,5 +13,42 @@ public static class Vector2IntExtensions
     public static float GetPercentageFor(this Vector2Int source, float value, bool clamp) => value.GetAsPercentageBetween(source, clamp);
 
     public static Vector3Int AxialToCube(this Vector2Int @this) => new Vector3Int(@this.x, @this.y, -@this.x-@this.y);
+    
+    
+    public static Vector2Int HexagonalRotationLeft(this Vector2Int @this) => HexagonalRotationLeft(@this, Vector2Int.zero);
+
+    public static Vector2Int HexagonalRotationLeft(this Vector2Int @this, Vector2Int pivot)
+    {
+        var coord = @this - pivot;
+
+        var old = coord;
+            
+        old.x -= (coord.y - (coord.y & 1)) / 2;
+
+        coord.x = -old.y;
+        coord.y = -(-old.x - old.y);
+
+        coord.x += (coord.y - (coord.y & 1)) / 2;
+
+        return coord + pivot;
+    }
+    
+    public static Vector2Int HexagonalRotationRight(this Vector2Int @this) => HexagonalRotationRight(@this, Vector2Int.zero);
+
+    public static Vector2Int HexagonalRotationRight(this Vector2Int @this, Vector2Int pivot)
+    {
+        var coord = @this - pivot;
+
+        var old = coord;
+            
+        old.x -= (coord.y - (coord.y & 1)) / 2;
+
+        coord.x = -(-old.x - old.y);
+        coord.y = -old.x;
+
+        coord.x += (coord.y - (coord.y & 1)) / 2;
+
+        return coord + pivot;
+    }
 
 }
