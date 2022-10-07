569f011d1fa5cd060ee74f0163bd4631fdba0c6e
Felipe Ishimine <felipeishimine@gmail.com>
2022-10-07 12:51:13 -0300

    BiDictionary Changes

--- Runtime/DataStructures/BiDictionary/BiDictionary.cs
+++ Runtime/DataStructures/BiDictionary/BiDictionary.cs
@@ -5,72 +5,84 @@ namespace DataStructures.BiDictionary
 {
     public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
     {
-        private readonly Dictionary<TKey, TValue> forward = new Dictionary<TKey, TValue>();
-        private readonly Dictionary<TValue, TKey> reverse = new Dictionary<TValue, TKey>();
+        private readonly Dictionary<TKey, TValue> _forward = new Dictionary<TKey, TValue>();
+        private readonly Dictionary<TValue, TKey> _reverse = new Dictionary<TValue, TKey>();
 
         public BiDictionary()
         {
-            KeyMap = forward;
-            ValueMap = reverse;
+            KeyMap = _forward;
+            ValueMap = _reverse;
         }
 
         public Dictionary<TKey, TValue> KeyMap { get; private set; }
         public Dictionary<TValue, TKey> ValueMap { get; private set; }
 
-        public int Count() => forward.Count;
+        public int Count() => _forward.Count;
 
+        public TValue this[TKey key]
+        {
+            get => KeyMap[key];
+            set => KeyMap[key] = value;
+        }
+
+        public TKey this[TValue key]
+        {
+            get => ValueMap[key];
+            set => ValueMap[key] = value;
+        }
+        
         public void Add(TKey key, TValue value)
         {
             if (key != null && value != null)
             {
-                forward.Add(key, value);
-                reverse.Add(value, key);
+                _forward.Add(key, value);
+                _reverse.Add(value, key);
             }
         }
 
         public bool ContainsKey(TKey key)
         {
-            return forward.ContainsKey(key);
+            return _forward.ContainsKey(key);
         }
 
         public bool ContainsValue(TValue value)
         {
-            return reverse.ContainsKey(value);
+            return _reverse.ContainsKey(value);
         }
 
         public bool RemoveKey(TKey key)
         {
-            var value = forward[key];
-            return value != null && reverse.ContainsKey(value)
-                && reverse.Remove(value) && forward.Remove(key);
+            var value = _forward[key];
+            return value != null && _reverse.ContainsKey(value)
+                && _reverse.Remove(value) && _forward.Remove(key);
         }
 
         public bool RemoveValue(TValue value)
         {
-            var key = reverse[value];
-            return key != null && forward.ContainsKey(key)
-                && forward.Remove(key) && reverse.Remove(value);
+            var key = _reverse[value];
+            return key != null && _forward.ContainsKey(key)
+                && _forward.Remove(key) && _reverse.Remove(value);
         }
 
         public IEnumerator<KeyValuePair<TValue, TKey>> GetValueEnumerator()
         {
-            return reverse.GetEnumerator();
+            return _reverse.GetEnumerator();
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            return forward.GetEnumerator();
+            return _forward.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return forward.GetEnumerator();
+            return _forward.GetEnumerator();
         }
 
         public void Clear()
         {
-            forward.Clear();
-            reverse.Clear();
+            _forward.Clear();
+            _reverse.Clear();
         }
     }
 }
