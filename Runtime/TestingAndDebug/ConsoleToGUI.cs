7b99955e3ee8fd2375322f68a8e90d1398f93f48
Ishimine <felipecalafate@hotmail.com>
2022-12-15 16:18:50 -0300

    Console To GUI

--- Runtime/TestingAndDebug/ConsoleToGUI.cs
+++ Runtime/TestingAndDebug/ConsoleToGUI.cs
@@ -1,42 +1,43 @@
 ﻿using UnityEngine;
-
+ 
 public class ConsoleToGUI : MonoBehaviour
 {
-	#if !UNITY_EDITOR
-	static string myLog = "";
-	private string output;
-	private string stack;
-	private bool showConsole;
+    string myLog = "*begin log";
+    string filename = "";
+    bool doShow;
+    int kChars = 700;
+    void OnEnable() { Application.logMessageReceived += Log; }
+    void OnDisable() { Application.logMessageReceived -= Log; }
 
-	void OnEnable()
-	{
-		Application.logMessageReceived += Log;
-	}
-
-	void OnDisable()
-	{
-		Application.logMessageReceived -= Log;
-	}
-
-	public void Log(string logString, string stackTrace, LogType type)
-	{
-		output = logString;
-		stack = stackTrace;
-		myLog = output + "\n\n" + myLog;
-		if (myLog.Length > 5000)
-		{
-			myLog = myLog.Substring(0, 4000);
-		}
-	}
-
-	void OnGUI()
-	{
-		showConsole = GUI.Toggle(new Rect(Screen.width - 30, Screen.height - 30, 30, 30), showConsole, "");
-
-		if (showConsole)
-		{
-			myLog = GUI.TextArea(new Rect(10, Screen.height / 2, Screen.width - 20, Screen.height), myLog);
-		}
-	}
-	#endif
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.KeypadDivide))
+            doShow = !doShow;
+    }
+    public void Log(string logString, string stackTrace, LogType type)
+    {
+        // for onscreen...
+        myLog = myLog + "\n" + logString;
+        if (myLog.Length > kChars) { myLog = myLog.Substring(myLog.Length - kChars); }
+ 
+        // for the file ...
+        if (filename == "")
+        {
+            string d = System.Environment.GetFolderPath(
+                System.Environment.SpecialFolder.Desktop) + "/YOUR_LOGS";
+            System.IO.Directory.CreateDirectory(d);
+            string r = Random.Range(1000, 9999).ToString();
+            filename = d + "/log-" + r + ".txt";
+        }
+        try { System.IO.File.AppendAllText(filename, logString + "\n"); }
+        catch { }
+    }
+ 
+    void OnGUI()
+    {
+        if (!doShow) { return; }
+        GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity,
+            new Vector3(Screen.width / 1200.0f, Screen.height / 800.0f, 1.0f));
+        GUI.TextArea(new Rect(10, 10, 540, 370), myLog);
+    }
 }
