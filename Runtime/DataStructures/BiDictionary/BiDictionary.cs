3c816e9e9dd1a67a56e70f6859fe641fac8f370f
Felipe Ishimine <felipeishimine@gmail.com>
2023-04-07 16:11:52 -0300

    BiDiccionary Fixes

--- Runtime/DataStructures/BiDictionary/BiDictionary.cs
+++ Runtime/DataStructures/BiDictionary/BiDictionary.cs
@@ -1,92 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace DataStructures.BiDictionary
 {
     public class BiDictionary<TKey, TValue> : IBiDictionary<TKey, TValue>
     {
-        private readonly Dictionary<TKey, TValue> _forward = new Dictionary<TKey, TValue>();
-        private readonly Dictionary<TValue, TKey> _reverse = new Dictionary<TValue, TKey>();
+        [ShowInInspector] private readonly Dictionary<TKey, TValue> _forward = new Dictionary<TKey, TValue>();
+        [ShowInInspector] private readonly Dictionary<TValue, TKey> _backward = new Dictionary<TValue, TKey>();
 
-        public BiDictionary()
-        {
-            KeyMap = _forward;
-            ValueMap = _reverse;
-        }
-
-        public Dictionary<TKey, TValue> KeyMap { get; private set; }
-        public Dictionary<TValue, TKey> ValueMap { get; private set; }
+        public Dictionary<TKey, TValue> KeyMap => _forward;
+        public Dictionary<TValue, TKey> ValueMap => _backward;
 
         public int Count() => _forward.Count;
 
         public TValue this[TKey key]
         {
             get => KeyMap[key];
-            set => KeyMap[key] = value;
+            set => Add(key,value);
         }
 
         public TKey this[TValue key]
         {
             get => ValueMap[key];
-            set => ValueMap[key] = value;
+            set => Add(value,key);
         }
         
         public void Add(TKey key, TValue value)
         {
-            if (key != null && value != null)
-            {
-                _forward.Add(key, value);
-                _reverse.Add(value, key);
-            }
+            _forward.Add(key, value);
+            _backward.Add(value, key);
         }
 
-        public bool ContainsKey(TKey key)
-        {
-            return _forward.ContainsKey(key);
-        }
+        public bool ContainsKey(TKey key) => _forward.ContainsKey(key);
 
-        public bool ContainsValue(TValue value)
-        {
-            return _reverse.ContainsKey(value);
-        }
+        public bool ContainsValue(TValue value) => _backward.ContainsKey(value);
 
-        public bool RemoveKey(TKey key)
-        {
-            var value = _forward[key];
-            return value != null && _reverse.ContainsKey(value)
-                && _reverse.Remove(value) && _forward.Remove(key);
-        }
+        public bool RemoveKey(TKey key) => _backward.Remove(_forward[key]) && _forward.Remove(key);
 
         public bool RemoveValue(TValue value)
         {
-            var key = _reverse[value];
-            return key != null && _forward.ContainsKey(key)
-                && _forward.Remove(key) && _reverse.Remove(value);
+            var key = _backward[value];
+
+            bool a = _forward.Remove(key);
+            bool b = _backward.Remove(value);
+
+            Debug.Log($"A:{a} B:{b}");
+
+            return a && b;
+            //return key != null && _forward.Remove(key) && _reverse.Remove(value);
         }
 
-        public IEnumerator<KeyValuePair<TValue, TKey>> GetValueEnumerator()
-        {
-            return _reverse.GetEnumerator();
-        }
+        public IEnumerator<KeyValuePair<TValue, TKey>> GetValueEnumerator() => _backward.GetEnumerator();
 
-        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-        {
-            return _forward.GetEnumerator();
-        }
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => _forward.GetEnumerator();
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return _forward.GetEnumerator();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => _forward.GetEnumerator();
 
         public void Clear()
         {
             _forward.Clear();
-            _reverse.Clear();
+            _backward.Clear();
         }
 
         public bool TryGetValue(TKey key, out TValue value) => _forward.TryGetValue(key, out value);
-        public bool TryGetValue(TValue key, out TKey value) => _reverse.TryGetValue(key, out value);
+        public bool TryGetValue(TValue key, out TKey value) => _backward.TryGetValue(key, out value);
     }
 }
