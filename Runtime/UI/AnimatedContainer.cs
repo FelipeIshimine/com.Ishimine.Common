3562f6615103f2c8c72fec396fd5d9ee978aea17
Ishimine <felipecalafate@hotmail.com>
2023-04-13 12:44:25 -0300

    AnimatedContainer Fix

--- Runtime/UI/AnimatedContainer.cs
+++ Runtime/UI/AnimatedContainer.cs
@@ -13,7 +13,7 @@ using UnityEngine.UI;
 /// Conteiner generico para animar canvas con 'coroutines'.
 /// Si la animacion por movimiento no encaja bien, revisar que el Root del container este bien posicionados
 /// </summary>
-public class AnimatedContainer : MonoBehaviour, ISelfValidator
+public class AnimatedContainer : MonoBehaviour, ISelfValidator, ISerializationCallbackReceiver
 {
     public InitState initializationState = InitState.Hide;
     [field:SerializeField, FormerlySerializedAs("timeType")] public TimeScale Time { get; private set; } = TimeScale.Unscaled;
@@ -31,7 +31,7 @@ public class AnimatedContainer : MonoBehaviour, ISelfValidator
     private RectTransform _parent;
     private RectTransform Parent => _parent ??= transform.parent as RectTransform;
 
-    [GetComponent,SerializeField, FormerlySerializedAs("_rectTransform")] private RectTransform rectTransform;
+    [SerializeField, FormerlySerializedAs("_rectTransform")] private RectTransform rectTransform;
     
     public enum Direction
     {
@@ -449,5 +449,13 @@ public class AnimatedContainer : MonoBehaviour, ISelfValidator
     }
 
     private void SetRectTransform(RectTransform nRectTransform) => this.rectTransform = nRectTransform;
+    public void OnBeforeSerialize()
+    {
+        if(rectTransform == null) SetRectTransform(transform as RectTransform);
+    }
+
+    public void OnAfterDeserialize()
+    {
+    }
 }
     
