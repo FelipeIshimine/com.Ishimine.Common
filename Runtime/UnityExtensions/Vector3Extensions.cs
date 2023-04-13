ce6d1093a1bc87662fce2f973520c6ee9265a2cf
Ishimine <felipecalafate@hotmail.com>
2023-04-13 10:13:38 -0300

    Ray to Point Distance

--- Runtime/UnityExtensions/Vector3Extensions.cs
+++ Runtime/UnityExtensions/Vector3Extensions.cs
@@ -15,20 +15,24 @@ public static class Vector3Extensions
     {
         float num1 = a.x - b.x;
         float num2 = a.z - b.z;
-        return (float) Math.Sqrt(num1 *  num1 + num2 *  num2);
+        return (float)Math.Sqrt(num1 * num1 + num2 * num2);
     }
-    
+
     public static float GetSqrDistanceXZ(this Vector3 a, Vector3 b)
     {
         float num1 = a.x - b.x;
         float num2 = a.z - b.z;
-        return num1 *  num1 +  num2 *  num2;
+        return num1 * num1 + num2 * num2;
     }
-    public static Vector3 GetDirection(this Vector3 startPos, Vector3 endPos) => GetDifference(startPos, endPos).normalized;
 
-    public static Vector3 GetDirectionXZ(this Vector3 startPos, Vector3 endPos) => GetDifferenceXZ(startPos,endPos).normalized;
-    
-    public static Vector3 GetDifferenceXZ(this Vector3 startPos, Vector3 endPos) => GetDifference(new Vector3(startPos.x,0,startPos.z), new Vector3(endPos.x,0,endPos.z));
+    public static Vector3 GetDirection(this Vector3 startPos, Vector3 endPos) =>
+        GetDifference(startPos, endPos).normalized;
+
+    public static Vector3 GetDirectionXZ(this Vector3 startPos, Vector3 endPos) =>
+        GetDifferenceXZ(startPos, endPos).normalized;
+
+    public static Vector3 GetDifferenceXZ(this Vector3 startPos, Vector3 endPos) =>
+        GetDifference(new Vector3(startPos.x, 0, startPos.z), new Vector3(endPos.x, 0, endPos.z));
 
     public static float GetMagnitude(this Vector3 startPos, Vector3 endPos) =>
         GetDifference(startPos, endPos).magnitude;
@@ -42,7 +46,9 @@ public static class Vector3Extensions
     public static float AsAngleXY(this Vector3 source) => Vector2Extensions.AsAngle(source);
 
     public static float GetAngleXY(this Vector3 startPos, Vector3 endPos) => ((Vector2)startPos).GetAngle(endPos);
-    public static float GetAngleXZ(this Vector3 startPos, Vector3 endPos) => (new Vector2(startPos.x,startPos.z)).GetAngle(new Vector2(endPos.x,endPos.z));
+
+    public static float GetAngleXZ(this Vector3 startPos, Vector3 endPos) =>
+        (new Vector2(startPos.x, startPos.z)).GetAngle(new Vector2(endPos.x, endPos.z));
 
     public static Vector3 SwapYZ(this Vector3 value)
     {
@@ -61,18 +67,20 @@ public static class Vector3Extensions
         return retValue;
     }
 
-    public static  Vector3 RotateByAxis(this Vector3 dir, Vector3 axis, float f) => Quaternion.AngleAxis(f,axis) * dir;
+    public static Vector3 RotateByAxis(this Vector3 dir, Vector3 axis, float f) => Quaternion.AngleAxis(f, axis) * dir;
 
-    public static Vector3 Clamp(this Vector3 value, Vector3 min, Vector3 max) => new Vector3(Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
+    public static Vector3 Clamp(this Vector3 value, Vector3 min, Vector3 max) => new Vector3(
+        Mathf.Clamp(value.x, min.x, max.x), Mathf.Clamp(value.y, min.y, max.y), Mathf.Clamp(value.z, min.z, max.z));
 
-    public static Vector3Int HexagonalRotationLeft(this Vector3Int @this) => HexagonalRotationLeft(@this, Vector3Int.zero);
+    public static Vector3Int HexagonalRotationLeft(this Vector3Int @this) =>
+        HexagonalRotationLeft(@this, Vector3Int.zero);
 
     public static Vector3Int HexagonalRotationLeft(this Vector3Int @this, Vector3Int pivot)
     {
         var coord = @this - pivot;
 
         var old = coord;
-            
+
         old.x -= (coord.y - (coord.y & 1)) / 2;
 
         coord.x = -old.y;
@@ -82,15 +90,16 @@ public static class Vector3Extensions
 
         return coord + pivot;
     }
-    
-    public static Vector3Int HexagonalRotationRight(this Vector3Int @this) => HexagonalRotationRight(@this, Vector3Int.zero);
+
+    public static Vector3Int HexagonalRotationRight(this Vector3Int @this) =>
+        HexagonalRotationRight(@this, Vector3Int.zero);
 
     public static Vector3Int HexagonalRotationRight(this Vector3Int @this, Vector3Int pivot)
     {
         var coord = @this - pivot;
 
         var old = coord;
-            
+
         old.x -= (coord.y - (coord.y & 1)) / 2;
 
         coord.x = -(-old.x - old.y);
@@ -100,7 +109,6 @@ public static class Vector3Extensions
 
         return coord + pivot;
     }
-    
+
+    public static Vector3 ClosestPointOnLine(this Vector3 point, Vector3 lineDirection, Vector3 targetPoint) => targetPoint + lineDirection * Vector3.Dot(point - targetPoint, lineDirection);
 }
-
-
