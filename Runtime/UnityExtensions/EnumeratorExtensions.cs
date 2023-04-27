42ce3308a9f9ca4243b96a42b4c62d2f02206151
Felipe Ishimine <felipeishimine@gmail.com>
2023-04-27 12:17:10 -0300

    Extended Coroutine

---
+++ Runtime/UnityExtensions/EnumeratorExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class EnumeratorExtensions
+{
+    public static ExtendedCoroutine<T> AsExtended<T>(this IEnumerator<T> enumerator, Action callback = null) => new(enumerator, callback);
+    public static ExtendedCoroutine AsExtended(this IEnumerator enumerator, Action callback = null) => new(enumerator, callback);
+}
---
+++ Runtime/UnityExtensions/ExtendedCoroutine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtendedCoroutine : IEnumerator
+{
+    public object Result { get; private set; }
+    private readonly IEnumerator _target;
+    private readonly Action _callback;
+
+    public ExtendedCoroutine(IEnumerator target, Action callback = null)
+    {
+        _target = target;
+        _callback = callback;
+    }
+    
+    public bool MoveNext()
+    {
+        if(_target.MoveNext()) 
+        {
+            Result = _target.Current;
+            Debug.Log($"Result:{Result}");
+            return true;
+        }
+        _callback?.Invoke();
+        return false;
+    }
+
+    public void Reset() { }
+
+    public object Current => _target.Current;
+}
+
+public class ExtendedCoroutine<T> : IEnumerator
+{
+    public T Result { get; private set; }
+    private readonly IEnumerator<T> _target;
+    private readonly Action _callback;
+
+    public ExtendedCoroutine(IEnumerator<T> target, Action callback = null)
+    {
+        _target = target;
+        _callback = callback;
+    }
+
+    public bool MoveNext()
+    {
+        if(_target.MoveNext()) 
+        {
+            Result = _target.Current;
+            Debug.Log($"Result:{Result}");
+            return true;
+        }
+        _callback?.Invoke();
+        return false;
+    }
+
+    public void Reset() { }
+
+    public object Current => _target.Current;
+}
