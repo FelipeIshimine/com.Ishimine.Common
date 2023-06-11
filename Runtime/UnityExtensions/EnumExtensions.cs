b03b6e64a23139fd578cc5fa33b39fe299224cc0
Ishimine <felipecalafate@hotmail.com>
2023-06-11 20:34:47 -0300

    Enum Extension

---
+++ Runtime/UnityExtensions/EnumExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class EnumExtensions
+{
+    public static IEnumerable<T> GetIndividualFlags<T>(this T flags) where T : Enum
+    {
+        foreach (T value in Enum.GetValues(typeof(T)))
+        {
+            if (flags.HasFlag(value))
+                yield return value;
+        }
+    }
+}
