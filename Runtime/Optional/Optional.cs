c4f6766ab9d599e6a249246bfc1e15fd22278959
Ishimine <felipecalafate@hotmail.com>
2023-07-07 16:07:11 -0300

    Optional

---
+++ Runtime/Optional/Optional.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+/// Requires Unity 2020.1+
+public struct Optional<T>
+{
+    [SerializeField] private bool enabled;
+    [SerializeField] private T value;
+
+    public bool Enabled => enabled;
+    public T Value => value;
+
+    public Optional(T initialValue)
+    {
+        enabled = true;
+        value = initialValue;
+    }
+    
+    public static implicit operator T(Optional<T> optional) => optional.value;
+    public static implicit operator bool(Optional<T> optional) => optional.enabled;
+}
