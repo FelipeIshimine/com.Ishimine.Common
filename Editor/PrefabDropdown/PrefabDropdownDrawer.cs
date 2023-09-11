ff6b28a104b01e7b5c41920389e2ea51ee3ae747
Felipe Ishimine <felipeishimine@gmail.com>
2023-09-11 00:41:29 -0300

    Prefab Dropdown Drawer Update

--- Editor/PrefabDropdown/PrefabDropdownDrawer.cs
+++ Editor/PrefabDropdown/PrefabDropdownDrawer.cs
@@ -16,59 +16,67 @@ public class PrefabDropdownDrawer : PropertyDrawer
     private List<Result> prefabList;
     private int selectedPrefabIndex = -1;
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+  public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+{
+    if (prefabList == null)
+        LoadPrefabs();
+
+    if (prefabList.Count == 0)
     {
-        if (prefabList == null)
-            LoadPrefabs();
-
-        if (prefabList.Count == 0)
-        {
-            EditorGUI.PropertyField(position, property, label);
-            return;
-        }
-
-        EditorGUI.BeginProperty(position, label, property);
-
-        // Calculate the label and dropdown widths
-        float labelWidth = EditorGUIUtility.labelWidth;
-        float dropdownWidth = position.width - labelWidth - EditorGUIUtility.singleLineHeight; // Subtract a button's width
-
-        // Split the position into label and dropdown portions
-        Rect labelRect = new Rect(position.x, position.y, labelWidth, position.height);
-        Rect dropdownRect = new Rect(position.x + labelWidth, position.y, dropdownWidth, position.height);
-        Rect buttonRect = new Rect(position.x + labelWidth + dropdownWidth, position.y, EditorGUIUtility.singleLineHeight, position.height);
-
-        // Draw the label using EditorGUI.PrefixLabel
-        EditorGUI.PrefixLabel(labelRect, label);
-
-        // Get the index of the currently selected prefab
-        string currentPrefabPath = property.objectReferenceValue != null
-            ? AssetDatabase.GetAssetPath(property.objectReferenceValue)
-            : null;
-
-        selectedPrefabIndex = prefabList.FindIndex(x => x.Path == currentPrefabPath);
-
-        // Show the dropdown for selecting a prefab
-        EditorGUI.BeginChangeCheck();
-        bool useFullPath = ((PrefabDropdownAttribute)attribute).showFullPath;
-        selectedPrefabIndex = EditorGUI.Popup(
-            dropdownRect,
-            selectedPrefabIndex,
-            prefabList.Select(x => useFullPath ? RemoveCommonPath(x.Path) : x.Prefab.name).ToArray()
-        );
-
-        if (EditorGUI.EndChangeCheck())
-        {
-            property.objectReferenceValue = selectedPrefabIndex >= 0 ? prefabList[selectedPrefabIndex].Prefab : null;
-        }
-
-        // Add a button to clear the field
-        if (GUI.Button(buttonRect, "X", EditorStyles.miniButton))
-        {
-            property.objectReferenceValue = null;
-        }
-        EditorGUI.EndProperty();
+        EditorGUI.PropertyField(position, property, label);
+        return;
     }
+
+    EditorGUI.BeginProperty(position, label, property);
+
+    // Calculate the label and dropdown widths
+    float labelWidth = EditorGUIUtility.labelWidth;
+    float dropdownWidth = 20; // Subtract a button's width
+
+    // Calculate the width of the property field
+    float propertyFieldWidth = position.width - labelWidth - dropdownWidth - EditorGUIUtility.singleLineHeight;
+
+    // Split the position into label, dropdown, property field, and button portions
+    Rect labelRect = new Rect(position.x, position.y, labelWidth, position.height);
+    Rect dropdownRect = new Rect(position.x + labelWidth, position.y, dropdownWidth, position.height);
+    Rect propertyFieldRect = new Rect(position.x + labelWidth + dropdownWidth, position.y, propertyFieldWidth, position.height);
+    Rect buttonRect = new Rect(position.x + labelWidth + dropdownWidth + propertyFieldWidth, position.y, EditorGUIUtility.singleLineHeight, position.height);
+
+    // Draw the label using EditorGUI.PrefixLabel
+    EditorGUI.PrefixLabel(labelRect, label);
+
+    // Get the index of the currently selected prefab
+    string currentPrefabPath = property.objectReferenceValue != null
+        ? AssetDatabase.GetAssetPath(property.objectReferenceValue)
+        : null;
+
+    selectedPrefabIndex = prefabList.FindIndex(x => x.Path == currentPrefabPath);
+
+    // Show the dropdown for selecting a prefab
+    EditorGUI.BeginChangeCheck();
+    bool useFullPath = ((PrefabDropdownAttribute)attribute).showFullPath;
+    selectedPrefabIndex = EditorGUI.Popup(
+        dropdownRect,
+        selectedPrefabIndex,
+        prefabList.Select(x => useFullPath ? RemoveCommonPath(x.Path) : x.Prefab.name).ToArray()
+    );
+
+    // Draw the property field
+    EditorGUI.PropertyField(propertyFieldRect, property, GUIContent.none);
+
+    if (EditorGUI.EndChangeCheck())
+    {
+        property.objectReferenceValue = selectedPrefabIndex >= 0 ? prefabList[selectedPrefabIndex].Prefab : null;
+    }
+
+    // Add a button to clear the field
+    if (GUI.Button(buttonRect, "X", EditorStyles.miniButton))
+    {
+        property.objectReferenceValue = null;
+    }
+    EditorGUI.EndProperty();
+}
+
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
