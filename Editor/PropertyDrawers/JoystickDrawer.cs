cebc286f937a9e3d224b0bb173e72ac66782c045
Felipe Ishimine <felipeishimine@gmail.com>
2023-10-10 10:34:21 -0300

    Joystick Update

--- Editor/PropertyDrawers/JoystickDrawer.cs
+++ Editor/PropertyDrawers/JoystickDrawer.cs
@@ -5,51 +5,76 @@ using UnityEngine;
 public class JoystickDrawer : PropertyDrawer
 {
     private const float LABEL_WIDTH = 200f;
-    
+    private const float HEIGHT = 50f;
+
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
         JoystickAttribute joystick = attribute as JoystickAttribute;
-        return EditorGUIUtility.singleLineHeight*2.5f + joystick.size;
+        return EditorGUIUtility.singleLineHeight * 3.5f + HEIGHT/2 ;
     }
 
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         JoystickAttribute joystick = attribute as JoystickAttribute;
-        float size = joystick.size;
 
         // Create a label for the vector property
         position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
+        int dimensions = property.propertyType == SerializedPropertyType.Vector2 ? 2 : 3;
+
+        Debug.Log(dimensions);
         // Calculate the position of the joystick
-        Rect joystickRect = new Rect(position.x + size/2, position.y + EditorGUIUtility.singleLineHeight, size, size);
+        Rect joystickRect = new Rect(position.x + HEIGHT / 2, position.y + EditorGUIUtility.singleLineHeight, HEIGHT, HEIGHT);
 
         // Calculate the position of the vertical slider
-        Rect sliderRectY = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight, size);
+        Rect sliderRectY = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight, HEIGHT);
 
         // Calculate the position of the horizontal slider
-        Rect sliderRectX = new Rect(position.x + joystickRect.width/2, position.y + size + EditorGUIUtility.singleLineHeight * 1.5f, size, EditorGUIUtility.singleLineHeight);
+        Rect sliderRectX = new Rect(position.x + joystickRect.width / 2, position.y + HEIGHT + EditorGUIUtility.singleLineHeight * 1.5f, HEIGHT, EditorGUIUtility.singleLineHeight);
 
-        Rect labelRect = new Rect(position.x + joystickRect.width + 60, position.y+EditorGUIUtility.singleLineHeight, LABEL_WIDTH * 2, position.height);
+        Rect labelRect = new Rect(position.x + joystickRect.width + 60, position.y + EditorGUIUtility.singleLineHeight, LABEL_WIDTH * 2, position.height);
 
         // Get the current value of the vector property
-        Vector2 value = property.vector2Value;
+        Vector3 value = property.propertyType == SerializedPropertyType.Vector3 ? property.vector3Value / joystick.MaxMagnitude : property.vector2Value / joystick.MaxMagnitude;
 
         // Draw the labels
-        DrawLabels(labelRect, ref value);
+        if (joystick.UseMultiplier)
+        {
+            DrawLabelsAndMultiplier(ref labelRect, ref value, ref joystick.MaxMagnitude, dimensions);
+        }
+        else
+        {
+            DrawSideLabels(ref labelRect, ref value, dimensions);
+        }
 
         // Draw the vertical slider
         value.y = GUI.VerticalSlider(sliderRectY, value.y, 1f, -1f);
 
         value.x = GUI.HorizontalSlider(sliderRectX, value.x, -1f, 1f);
 
+	    if (dimensions == 3)
+	    {
+		    Rect sliderRectZ = new Rect(position.x + joystickRect.width + EditorGUIUtility.singleLineHeight*2, position.y + EditorGUIUtility.singleLineHeight, EditorGUIUtility.singleLineHeight, HEIGHT);
+		    value.z = GUI.VerticalSlider(sliderRectZ, value.z, 1f, -1f);
+	    }
+
         // Draw the joystick
-        value = DrawJoystick(joystickRect, value, size);
+        value = DrawJoystick(joystickRect, value, HEIGHT);
 
         // Set the vector property to the new value
-        property.vector2Value = value;
+        if (property.propertyType == SerializedPropertyType.Vector3)
+        {
+            property.vector3Value = value * joystick.MaxMagnitude;
+        }
+        else
+        {
+            property.vector2Value = value * joystick.MaxMagnitude;
+        }
+
+	    position.x *= .5f;
     }
 
-    private Vector2 DrawJoystick(Rect position, Vector2 value, float size)
+    private Vector3 DrawJoystick(Rect position, Vector3 value, float size)
     {
         value.y *= -1;
         // Calculate the center position of the joystick
@@ -86,28 +111,56 @@ public class JoystickDrawer : PropertyDrawer
             Vector2 direction = (mousePosition - center).normalized;
             float distance = Vector2.Distance(mousePosition, center);
             float clampedDistance = Mathf.Clamp(distance, 0f, maxDistance);
-            value = direction * (clampedDistance / maxDistance);
+            var z = value.z;
+            value = new Vector3(direction.x, direction.y) * (clampedDistance / maxDistance);
+            value.z = z;
         }
 
         value.y *= -1;
         return value;
     }
-    
-    private void DrawLabels(Rect position, ref Vector2 value)
-    {
-        Rect yLabelRect = new Rect(position.x, position.y, 100, EditorGUIUtility.singleLineHeight);
-        EditorGUI.LabelField(yLabelRect, "Y:");
 
-        Rect yFieldRect = new Rect(position.x + 20, position.y, 60, EditorGUIUtility.singleLineHeight);
-        value.y = EditorGUI.FloatField(yFieldRect, value.y);
+    private void DrawSideLabels(ref Rect position, ref Vector3 value, int dimensions)
+    {
+	    value = value.Clamp(-Vector3.one,Vector3.one);
+        for (int i = 0; i < dimensions; i++)
+        {
+            string label = i == 0 ? "X:" : i == 1 ? "Y:" : "Z:";
+            Rect labelRect = new Rect(position.x, position.y, 100, EditorGUIUtility.singleLineHeight);
+            EditorGUI.LabelField(labelRect, label);
+
+            Rect fieldRect = new Rect(position.x + 20, position.y, 60, EditorGUIUtility.singleLineHeight);
+
+            switch (i)
+            {
+                case 0:
+                    value.x = EditorGUI.FloatField(fieldRect, value.x);
+                    break;
+                case 1:
+                    value.y = EditorGUI.FloatField(fieldRect, value.y);
+                    break;
+                case 2:
+                    value.z = EditorGUI.FloatField(fieldRect, value.z);
+                    break;
+            }
+
+            position.y += EditorGUIUtility.singleLineHeight;
+        }
+    }
+
+    private void DrawLabelsAndMultiplier(ref Rect position, ref Vector3 value, ref float multiplier, int dimensions)
+    {
+        Rect multLabelRect = new Rect(position.x, position.y, 100, EditorGUIUtility.singleLineHeight);
+        EditorGUI.LabelField(multLabelRect, "M:");
+
+        Rect multFieldRect = new Rect(position.x + 20, position.y, 60, EditorGUIUtility.singleLineHeight);
+
+        if (multiplier == 0) multiplier = .01f;
+
+        multiplier = EditorGUI.FloatField(multFieldRect, multiplier);
 
         position.y += EditorGUIUtility.singleLineHeight;
 
-        Rect xLabelRect = new Rect(position.x, position.y, 100, EditorGUIUtility.singleLineHeight);
-        EditorGUI.LabelField(xLabelRect, "X:");
-
-        Rect xFieldRect = new Rect(position.x + 20, position.y, 60, EditorGUIUtility.singleLineHeight);
-        value.x = EditorGUI.FloatField(xFieldRect, value.x);
+        DrawSideLabels(ref position, ref value, dimensions);
     }
-    
 }
--- Runtime/Attributes/JoystickAttribute.cs
+++ Runtime/Attributes/JoystickAttribute.cs
@@ -2,11 +2,24 @@
 
 public class JoystickAttribute : PropertyAttribute
 {
-    public float size = 50;
+	public readonly bool UseMultiplier;
+	public float MaxMagnitude;
 
-    public JoystickAttribute() { }
-    public JoystickAttribute(float size)
-    {
-        this.size = size;
-    }
+	public JoystickAttribute(bool useMultiplier, float maxMagnitude)
+	{
+		this.UseMultiplier = useMultiplier;
+		this.MaxMagnitude = maxMagnitude;
+	}
+	
+	public JoystickAttribute(bool useMultiplier)
+	{
+		this.UseMultiplier = useMultiplier;
+		this.MaxMagnitude = 1;
+	}
+	
+	public JoystickAttribute()
+	{
+		this.UseMultiplier = false;
+		this.MaxMagnitude = 1;
+	}
 }
