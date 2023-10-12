a43d14f065b7c28a8c0fff93b8711b958961732d
Felipe Ishimine <felipeishimine@gmail.com>
2023-10-11 23:10:06 -0300

    ParticlesSystemExtensions

---
+++ Runtime/UnityExtensions/ParticleSystemExtensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class ParticleSystemExtensions
+{
+	public static void SetStopAction(this ParticleSystem particleSystem, ParticleSystemStopAction stopAction)
+	{
+		var particleSystemMain = particleSystem.main;
+		particleSystemMain.stopAction = stopAction;
+	}
+}
