46288b5a29a9332e04a6965328ba732d62bd236f
Ishimine <felipecalafate@hotmail.com>
2023-10-19 11:24:24 -0300

    DrawArrow Fix

--- Runtime/UnityExtensions/DrawArrow.cs
+++ Runtime/UnityExtensions/DrawArrow.cs
@@ -4,6 +4,10 @@ public static class DrawArrow
 {
     public static void ForGizmo(Vector3 pos, Vector3 direction, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
     {
+	    if (direction.sqrMagnitude == 0)
+	    {
+		    return;
+	    }
         Gizmos.DrawRay(pos, direction);
        
         Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0,180+arrowHeadAngle,0) * new Vector3(0,0,1);
@@ -14,6 +18,10 @@ public static class DrawArrow
  
     public static void ForGizmo(Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
     {
+	    if (direction.sqrMagnitude == 0)
+	    {
+		    return;
+	    }
         Gizmos.color = color;
         Gizmos.DrawRay(pos, direction);
        
@@ -25,6 +33,11 @@ public static class DrawArrow
  
     public static void ForDebug(Vector3 pos, Vector3 direction, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
     {
+	    if (direction.sqrMagnitude == 0)
+	    {
+		    return;
+	    }
+	    
         Debug.DrawRay(pos, direction);
        
         Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0,180+arrowHeadAngle,0) * new Vector3(0,0,1);
@@ -34,6 +47,10 @@ public static class DrawArrow
     }
     public static void ForDebug(Vector3 pos, Vector3 direction, Color color, float arrowHeadLength = 0.25f, float arrowHeadAngle = 20.0f)
     {
+	    if (direction.sqrMagnitude == 0)
+	    {
+		    return;
+	    }
         Debug.DrawRay(pos, direction, color);
        
         Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0,180+arrowHeadAngle,0) * new Vector3(0,0,1);
