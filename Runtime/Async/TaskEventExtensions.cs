b00616f403bde3699a6ad45f075c4676c1f77dfa
Ishimine <felipecalafate@hotmail.com>
2023-10-23 10:33:28 -0300

    TaskEventExtensions + UniTaskEventExtensions

---
+++ Runtime/Async/TaskEventExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public static class TaskEventExtensions
+{
+	public static Task InvokeAsync<TSource, TEventArgs>(this Func<TSource, TEventArgs, Task> handlers,
+	                                                    TSource source,
+	                                                    TEventArgs args)
+	{
+		if (handlers != null)
+		{
+			var delegates = handlers.GetInvocationList();
+			Task[] tasks = new Task[delegates.Length];
+			for (var index = 0; index < delegates.Length; index++)
+			{
+				tasks[index] = ((Func<TSource, TEventArgs, Task>)delegates[index]).Invoke(source,args);
+			}
+			return Task.WhenAll(tasks);
+		}
+		return Task.CompletedTask;
+	}
+	
+	public static Task InvokeAsync<TEventArgs>(this Func<TEventArgs, Task> handlers, TEventArgs args)
+	{
+		if (handlers != null)
+		{
+			var delegates = handlers.GetInvocationList();
+			Task[] tasks = new Task[delegates.Length];
+			for (var index = 0; index < delegates.Length; index++)
+			{
+				tasks[index] = ((Func<TEventArgs, Task>)delegates[index]).Invoke(args);
+			}
+			return Task.WhenAll(tasks);
+		}
+		return Task.CompletedTask;
+	}
+}
---
+++ Runtime/Async/UniTaskEventExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+
+public static class UniTaskEventExtensions
+{
+	public static UniTask InvokeAsync<TSource, TEventArgs>(this Func<TSource, TEventArgs, UniTask> handlers,
+	                                                       TSource source,
+	                                                       TEventArgs args)
+	{
+		if (handlers != null)
+		{
+			var delegates = handlers.GetInvocationList();
+			UniTask[] tasks = new UniTask[delegates.Length];
+			for (var index = 0; index < delegates.Length; index++)
+			{
+				tasks[index] = ((Func<TSource, TEventArgs, UniTask>)delegates[index]).Invoke(source,args);
+			}
+			return UniTask.WhenAll(tasks);
+		}
+		return UniTask.CompletedTask;
+	}
+	
+	public static UniTask InvokeAsync<TEventArgs>(this Func<TEventArgs, UniTask> handlers, TEventArgs args)
+	{
+		if (handlers != null)
+		{
+			var delegates = handlers.GetInvocationList();
+			UniTask[] tasks = new UniTask[delegates.Length];
+			for (var index = 0; index < delegates.Length; index++)
+			{
+				tasks[index] = ((Func<TEventArgs, UniTask>)delegates[index]).Invoke(args);
+			}
+			return UniTask.WhenAll(tasks);
+		}
+		return UniTask.CompletedTask;
+	}
+}
