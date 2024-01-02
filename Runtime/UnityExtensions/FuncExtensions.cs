242954257b7f60991d1f0c3f840a8369e3e4b832
Ishimine <felipecalafate@hotmail.com>
2024-01-02 10:52:04 -0300

    Fixes

---
+++ Runtime/UnityExtensions/FuncExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class FuncExtensions
+{
+	public static IEnumerable<T> EnumerableInvoke<T>(this Func<T> handler)
+	{
+		if (handler != null)
+		{
+			var delegates = handler.GetInvocationList();
+			for (var index = 0; index < delegates.Length; index++)
+			{
+				yield return ((Func<T>)delegates[index]).Invoke();
+			}
+		}
+	}
+}
