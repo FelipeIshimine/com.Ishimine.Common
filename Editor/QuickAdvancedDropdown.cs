ffd4b9d8be61751d52fe8122907bd1b74ad14af1
Ishimine <felipecalafate@hotmail.com>
2024-02-06 23:54:54 -0300

    Quick Advance Dropdown

---
+++ Editor/QuickAdvancedDropdown.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEditor.IMGUI.Controls;
+
+public class QuickAdvancedDropdown : AdvancedDropdown
+{
+	private readonly string[] labels;
+	private readonly Action<int> callback;
+
+	public QuickAdvancedDropdown(string[] labels, Action<int> callback) : base(new AdvancedDropdownState())
+	{
+		this.labels = labels;
+		this.callback = callback;
+	}
+
+	protected override AdvancedDropdownItem BuildRoot()
+	{
+		AdvancedDropdownItem root = new AdvancedDropdownItem("Scenes");
+
+		for (int i = 0; i < labels.Length; i++)
+		{
+			root.AddChild(new AdvancedDropdownItem(labels[i])
+			{
+				id = i
+			});
+		}
+		return root;
+	}
+
+	protected override void ItemSelected(AdvancedDropdownItem item) => callback.Invoke(item.id);
+}
