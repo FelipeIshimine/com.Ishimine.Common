0c2762248b98ffbec9a1c8f78eac9e652bf5b72d
Ishimine <felipecalafate@hotmail.com>
2024-03-14 13:09:00 -0300

    namespace fix

--- Editor/NavMesh/NavMeshMaskDrawer.cs
+++ Editor/NavMesh/NavMeshMaskDrawer.cs
@@ -1,26 +1,23 @@
 using UnityEditor;
 using UnityEngine;
 
-namespace NavMesh
+[CustomPropertyDrawer(typeof(NavMeshMaskAttribute))]
+public class NavMeshMaskDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty serializedProperty, GUIContent label)
 	{
-		[CustomPropertyDrawer(typeof(NavMeshMaskAttribute))]
-		public class NavMeshMaskDrawer : PropertyDrawer
-		{
-			public override void OnGUI(Rect position, SerializedProperty serializedProperty, GUIContent label)
-			{
-				var width = position.width;
-				position.width = EditorGUIUtility.labelWidth;
-				EditorGUI.PrefixLabel(position, label);
+		var width = position.width;
+		position.width = EditorGUIUtility.labelWidth;
+		EditorGUI.PrefixLabel(position, label);
          
-				var areaNames  = GameObjectUtility.GetNavMeshAreaNames();
-				var mask = serializedProperty.intValue;
-				position.x += EditorGUIUtility.labelWidth;
-				position.width = width - EditorGUIUtility.labelWidth;
+		var areaNames  = GameObjectUtility.GetNavMeshAreaNames();
+		var mask = serializedProperty.intValue;
+		position.x += EditorGUIUtility.labelWidth;
+		position.width = width - EditorGUIUtility.labelWidth;
  
-				EditorGUI.BeginChangeCheck();
-				mask = EditorGUI.MaskField(position, mask, areaNames);
-				if (EditorGUI.EndChangeCheck())
-					serializedProperty.intValue = mask;
-			}
-		}
+		EditorGUI.BeginChangeCheck();
+		mask = EditorGUI.MaskField(position, mask, areaNames);
+		if (EditorGUI.EndChangeCheck())
+			serializedProperty.intValue = mask;
 	}
+}
