4d7933e08459759c9e6ceeff230ea836a59b4fde
Ishimine <felipecalafate@hotmail.com>
2024-03-26 10:50:25 -0300

    Double Click Manipulator

---
+++ Editor/Manipulators/DoubleClickManipulator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Manipulators
+{
+	public class DoubleClickManipulator : MouseManipulator
+	{
+		public float doubleClickTimeThreshold = 0.3f; // Adjust this threshold as needed
+		private float lastClickTime = 0f;
+		public Action Callback { get; set; }
+
+		public DoubleClickManipulator()
+		{
+			activators.Add(new ManipulatorActivationFilter { button = MouseButton.LeftMouse });
+		}
+
+		protected override void RegisterCallbacksOnTarget()
+		{
+			target.RegisterCallback<MouseDownEvent>(OnMouseDown);
+		}
+
+		protected override void UnregisterCallbacksFromTarget()
+		{
+			target.UnregisterCallback<MouseDownEvent>(OnMouseDown);
+		}
+
+		private void OnMouseDown(MouseDownEvent evt)
+		{
+			if (evt.clickCount == 2 && Time.time - lastClickTime < doubleClickTimeThreshold)
+			{
+				lastClickTime = 0f;
+				evt.StopImmediatePropagation();
+				DoubleClickAction();
+			}
+			else
+			{
+				lastClickTime = Time.time;
+			}
+		}
+
+		private void DoubleClickAction() => Callback?.Invoke();
+	}
+}
