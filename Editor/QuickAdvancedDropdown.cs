5b9ed5d6a4a10344b4b97992324d6c1aac741d58
Ishimine <felipecalafate@hotmail.com>
2024-09-12 18:56:50 -0300

    Quick Advanced dropdown builder

--- Editor/QuickAdvancedDropdown.cs
+++ Editor/QuickAdvancedDropdown.cs
@@ -1,33 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEditor.IMGUI.Controls;
+using UnityEngine;
 
 public class QuickAdvancedDropdown : AdvancedDropdown
 {
-	private readonly string title;
-	private readonly string[] labels;
+	private readonly AdvancedDropdownItem root;
 	private readonly Action<int> callback;
-	private readonly char splitCharacter;
-
-	private Dictionary<string, AdvancedDropdownItem> pathToDropdownItems =
-		new Dictionary<string, AdvancedDropdownItem>();
 	
-	public QuickAdvancedDropdown(string title, string[] labels, Action<int> callback, char splitCharacter = '/') : base(new AdvancedDropdownState())
+	public QuickAdvancedDropdown(AdvancedDropdownItem root, Action<int> action) : base(new AdvancedDropdownState())
 	{
-		this.title = title;
-		this.labels = labels;
-		this.callback = callback;
-		this.splitCharacter = splitCharacter;
+		this.root = root;
+		callback = action;
 	}
 
 	protected override AdvancedDropdownItem BuildRoot()
 	{
+		return root;
+	}
+	
+	protected override void ItemSelected(AdvancedDropdownItem item) => callback.Invoke(item.id);
+}
+
+
+public class AdvancedDropdownBuilder
+{
+	private string title;
+	private List<string> labels = new();
+	private int startingID;
+	private Action<int> callback;
+	private char splitCharacter = '/';
+
+	public AdvancedDropdownBuilder WithTitle(string title)
+	{
+		 this.title = title;
+		 return this;
+	}
+
+	public AdvancedDropdownBuilder AddElements(IEnumerable<string> elements, out List<int> indices)
+	{
+		indices = new List<int>();
+		foreach (string element in elements)
+		{
+			labels.Add(element);
+			indices.Add(labels.Count-1);
+		}
+		return this;
+	}
+	
+	public AdvancedDropdownBuilder AddElement(string element, out int index)
+	{
+		 labels.Add(element);
+		 index = labels.Count - 1;
+		 return this;
+	}
+
+	public AdvancedDropdownBuilder SetSplitCharacter(char splitChar)
+	{
+		 this.splitCharacter = splitChar;
+		 return this;
+	}
+
+	public AdvancedDropdownBuilder SetCallback(Action<int> callback)
+	{
+		this.callback = callback;
+		return this;
+	}
+
+	public AdvancedDropdown Build()
+	{
+		Dictionary<string, AdvancedDropdownItem> pathToDropdownItems = new Dictionary<string, AdvancedDropdownItem>();
 		AdvancedDropdownItem root = new AdvancedDropdownItem(title);
-		
-		for (int i = 0; i < labels.Length; i++)
+		AdvancedDropdownItem startingItem = null;
+		for (int i = 0; i < labels.Count; i++)
 		{
 			var split = labels[i].Split(splitCharacter);
-
 			var previous = root;
 			for (var j = 0; j < split.Length; j++)
 			{
@@ -38,19 +86,42 @@ public class QuickAdvancedDropdown : AdvancedDropdown
 					{
 						id = i
 					};
+
+					if (i == startingID)
+					{
+						startingItem = previous;
+					}
 					previous.AddChild(item);
 				}
 				previous = item;
-				//item = root.AddChild(new AdvancedDropdownItem(s));
 			}
-
-			/*root.AddChild(new AdvancedDropdownItem(labels[i])
-			{
-				id = i
-			});*/
 		}
-		return root;
+		return new QuickAdvancedDropdown(root,callback);
 	}
 
-	protected override void ItemSelected(AdvancedDropdownItem item) => callback.Invoke(item.id);
+	public AdvancedDropdownBuilder SetStartingId(int i)
+	{
+		startingID = i;
+		return this;
+	}
+	
+	// Reflection method to set the initial selection
+	private void SetInitialSelection(AdvancedDropdown dropdown, AdvancedDropdownItem startingItem)
+	{
+		if (startingItem == null) return;
+
+		// Use reflection to access the internal `_selectedItem` field of `AdvancedDropdown`
+		var dropdownType = typeof(AdvancedDropdown);
+		var selectedItemField = dropdownType.GetField("itemSelected", BindingFlags.NonPublic | BindingFlags.Instance);
+		if (selectedItemField != null)
+		{
+			// Set the initial selection to the starting item
+			selectedItemField.SetValue(dropdown, startingItem);
+			Debug.Log($"Initial item set via reflection: {startingItem.name}");
+		}
+		else
+		{
+			Debug.LogError("Could not find the internal 'itemSelected' field via reflection.");
+		}
+	}
 }
